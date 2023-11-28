23d803b0ab35bbef7973ea026a3c053863e8e741
tobi <tobiemba@gmail.com>
2023-11-28 14:14:51 +0100

    ergf

--- pages/signin/signin.js
+++ pages/signin/signin.js
@@ -1,15 +1,29 @@
+// Fonction pour vérifier la validité de l'e-mail
+function verifyInputs() {
+   // Récupérer la valeur de l'input e-mail
+   let email = document.getElementById("email").value;
+ 
+   // Expression régulière pour vérifier le format de l'e-mail
+   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ 
+   // Vérifier si l'e-mail correspond au format attendu
+   if (emailPattern.test(email)) {
+      let modal = document.querySelector(".loadModal")
+      modal.style.display ="block"
+      localStorage.setItem("email", email);
 
-let modal = document.querySelector(".loadModal");
-let btn = document.querySelector(".btn");
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-function EmailSend(){
-let input = document.querySelector("#email").value;
-
-}
+      setTimeout(() => {
+         modal.style.display = 'none'; // Masquage du modal
+         window.location.href = '../../pages/sendCode/send.html'; 
+       }, 5000);
+   } else {
+      alert("L'e-mail est invalide !");
+   }
+ }
 
 
+
+
+ 
+ 
+ 
